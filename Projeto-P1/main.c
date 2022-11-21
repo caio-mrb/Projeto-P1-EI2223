06c82068a2a1d89454fd06e1f1007bc0ef303bbd
caio-mrb <caiomaxwel@hotmail.com>
2022-11-21 21:04:53 +0000

    Struct laptop and date created

--- Projeto-P1/main.c
+++ Projeto-P1/main.c
@@ -1,14 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+
+struct date {
+    int day;
+    int month;
+    int year;
+
+};
+
+struct laptop {
+    int id;
+    char processor[3];
+    int ram;
+    char state[10];
+    char location[10];
+    struct date date;
+
+    };
 
 int main()
 {
-    printf("Hello world!\n");
-    printf("Teste");
-    printf("Testeeee");
-    printf("projeto P1");
-    printf("HOLA!");
-    printf("Teste2");
-    printf("arrozdoce");
+
+    struct laptop laptop;
+    struct date date;
+    laptop.date.day = 1;
+
+    printf("%d",laptop.date.day);
+
     return 0;
 }
